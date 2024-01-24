2e5704d81a7aa50e7fb80a243c2783be182fb609
Chirag <chirag@ledworldlighting.com>
2024-01-24 14:02:49 -0700

    exercise 1.6 completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react"
 
+const Header = ({heading}) => <h1>{heading}</h1> 
+
+const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+
+const Feedback = (props) => {
+  return(
+    <>
+      <p>Good {props.good}</p>
+      <p>Bad {props.bad}</p>
+      <p>Neutral {props.neutral}</p>
+    </>
+  )
+}
 
 const App = () => {
+  
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+
   return(
     <div>
-      
+      <Header heading="Give feedback" />
+      <Button handleClick={handleGood} text="Good"></Button>
+      <Button handleClick={handleBad} text="Bad"></Button>
+      <Button handleClick={handleNeutral} text="Neutral"></Button>
+      <Header heading="Statastics" />
+      <Feedback good={good} bad={bad} neutral={neutral} />
+
     </div>
   )
 }
