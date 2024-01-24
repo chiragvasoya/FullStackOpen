ef572db4ea39cb4b25052321c31784177e4b1b34
Chirag <chirag@ledworldlighting.com>
2024-01-24 14:19:15 -0700

    exercise 1.7 completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,6 +10,9 @@ const Feedback = (props) => {
       <p>Good {props.good}</p>
       <p>Bad {props.bad}</p>
       <p>Neutral {props.neutral}</p>
+      <p>All {props.all}</p>
+      <p>Average {props.all === 0 ? 0 : props.average/props.all}</p>
+      <p>Positive {props.all === 0 ? 0 : props.good/props.all}%</p>
     </>
   )
 }
@@ -19,17 +22,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(average + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(average -1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
 
@@ -40,7 +50,7 @@ const App = () => {
       <Button handleClick={handleBad} text="Bad"></Button>
       <Button handleClick={handleNeutral} text="Neutral"></Button>
       <Header heading="Statastics" />
-      <Feedback good={good} bad={bad} neutral={neutral} />
+      <Feedback good={good} bad={bad} neutral={neutral} all={all} average={average} />
 
     </div>
   )
