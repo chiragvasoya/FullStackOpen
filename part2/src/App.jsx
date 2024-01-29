b720a19365ef30861e2af3d29506aa7f4e0cc687
Chirag <chirag@ledworldlighting.com>
2024-01-29 14:33:30 -0700

    initial commit

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,9 +1,71 @@
 
+// Header Component
+const Header = ({course}) => <h1>{course.name}</h1>
 
-const App = () => {
+// Content Component
+const Content = ({parts}) => {
+  
   return(
     <>
+      { parts.map((part, i) => <Part key={i} part={part.name} exercise={part.exercises} /> ) }
+      
     </>
+  )
+}
+
+// Total Component
+const Total = ({exercises}) => {
+  return ( 
+    <>
+    <p>Number of exercises { exercises[0].exercises + exercises[1].exercises + exercises[2].exercises }</p>
+    </>
+  )
+}
+
+// Part Component
+
+const Part = ({part, exercise}) => {
+  return(
+    <>
+     <p>
+         {part} {exercise}
+      </p>
+    </>
+  )
+}
+
+
+const App = () => {
+  
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return(
+    <div>
+      <Header course={course}></Header>
+      <Content parts={course.parts}  ></Content>
+      <Total exercises={course.parts} /> 
+      
+    </div>
   )
 }
 
