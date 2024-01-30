032abeed341c4fb64bb7d94fcb1f8b419f01bc2e
Chirag <chirag@ledworldlighting.com>
2024-01-30 14:47:23 -0700

    Expercise 2.6 completed

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,9 +1,37 @@
-
+import { useState } from "react"
 
 const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addName = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
   return(
-    <>
-    </>
+    <div>
+      <h2>Title 1</h2>
+      <form onSubmit={addName}>
+      <input type="text" onChange={handleNewName} value={newName} />
+      <button type="submit">Add</button>
+      </form>
+      <h2>Title 2</h2>
+      <ul>
+        { persons.map(person => <li key={person.name}>{person.name}</li> ) }
+      </ul>
+    </div>
   )
 }
 
